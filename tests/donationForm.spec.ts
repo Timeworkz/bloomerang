0b7ff107595709deeb372470d2baf6267e57c26e
domalbergo <42678506+domalbergo@users.noreply.github.com>
2025-06-06 10:29:32 -0400

    Update donationForm.spec.ts

--- tests/donationForm.spec.ts
+++ tests/donationForm.spec.ts
@@ -46,27 +46,8 @@ test.describe('Bloomerang Donation Form - Basic Functionality', () => {
         await expect(donateButton).toHaveText('Donate $25.00', {timeout: 2000});
     });
 
-    test('Verify $50 donation with Savings shows correct button text', async ({ page }) => {
-  
-
-        await page.locator('input[name="radio-set-1"][value="50"]').click(); // Select $50
-
-        await page.locator('input[data-testid="contact.firstName"]').fill('Poorly');
-        await page.locator('input[data-testid="contact.lastName"]').fill('Written');
-        await page.locator('input[data-testid="contact.email"]').fill('poor@example.com');
-        await page.selectOption('select[data-testid="billingAddress.country"]', { label: 'United States' });
-        await page.locator('input[data-testid="billingAddress.street"]').fill('1 Refactor Lane');
-        await page.locator('input[data-testid="billingAddress.city"]').fill('Testville');
-        await page.selectOption('select[data-testid="billingAddress.state"]', { label: 'Indiana' });
-        await page.locator('input[data-testid="billingAddress.postalCode"]').fill('54321');
-
-        await page.locator('input[name="radio-set-3"][value="bankAccount"][data-testid="transaction.payment.type.bankAccount.savings"]').click(); // Select Savings
-
-        const donateButton = page.locator('button[data-testid="button.submit.text"]');
-        await expect(donateButton).toHaveText('Donate $50.00');
-    });
-
-    // --- Candidate should add their tests below this line based on their test plan ---
+   // --- Candidate should add their tests below this line based on their test plan ---
     // --- These tests should focus on the User Story: "Display Donation Total on Donate Button" ---
+ 
 
 });
